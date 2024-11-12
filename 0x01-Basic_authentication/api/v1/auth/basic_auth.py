fc95b4e047e33d0acd79edfb41237b7fdb28ef36
beloveyebaoh <beloveyeboah@gmail.com>
2024-11-12 23:28:18 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,12 +5,15 @@ basic authentification for the API
 
 from api.v1.auth.auth import Auth
 
+
 class BasicAuth(Auth):
     """Class for Basic Authentication"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """
-        Extracts the Base64 part of the Authorization header for Basic Authentication.
+        Extracts the Base64 part of the Authorization
+        header for Basic Authentication.
         :param authorization_header: Authorization header value
         :return: Base64 part of the Authorization header or None
         """
@@ -23,3 +26,17 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
