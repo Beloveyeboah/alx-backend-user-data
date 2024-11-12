710911fc84779f9e36f7d5a8f4e845f50b7c9111
beloveyebaoh <beloveyeboah@gmail.com>
2024-11-12 23:19:04 +0000

    basic extra

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,21 @@ basic authentification for the API
 
 from api.v1.auth.auth import Auth
 
-
 class BasicAuth(Auth):
-    """Empty class for Basic Authentication"""
-    pass
+    """Class for Basic Authentication"""
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header for Basic Authentication.
+        :param authorization_header: Authorization header value
+        :return: Base64 part of the Authorization header or None
+        """
+
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[len("Basic "):]
