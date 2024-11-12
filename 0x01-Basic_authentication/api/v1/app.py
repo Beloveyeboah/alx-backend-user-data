fc22557b5c616fdb81b39e8a3c788495af9dc668
beloveyebaoh <beloveyeboah@gmail.com>
2024-11-12 22:11:52 +0000

    errors handling

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,7 +22,7 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error):
+def unauthorized(error) -> str:
     """Custom error handler for 401 Unauthorized"""
     return jsonify({"error": "Unauthorized"}), 401
 
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,8 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'],  strict_slashes=False)
-def unauthorized():
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
     """Endpoint to raise a 401 error"""
+
     abort(401)
