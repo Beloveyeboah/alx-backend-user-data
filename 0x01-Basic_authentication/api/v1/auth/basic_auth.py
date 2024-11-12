4ad6a78835799ede4e9599ec3debbd7b6844c066
beloveyebaoh <beloveyeboah@gmail.com>
2024-11-12 23:44:53 +0000

    new basic

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,42 +1,58 @@
 #!/usr/bin/env python3
 """
-basic authentification for the API
+Module for authentication using Basic auth
 """
 
+
+from typing import TypeVar
 from api.v1.auth.auth import Auth
 import base64
-from typing import TypeVar
+
+from models.user import User
 
 
 class BasicAuth(Auth):
-    """Class for Basic Authentication"""
+    """_summary_
+    """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """
-        Extracts the Base64 part of the Authorization
-        header for Basic Authentication.
-        :param authorization_header: Authorization header value
-        :return: Base64 part of the Authorization header or None
-        """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """_summary_
 
+        Args:
+                 authorization_header (str): _description_
+
+        Returns:
+                 str: _description_
+        """
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
             return None
-        if not authorization_header.startswith("Basic "):
+        if not authorization_header.startswith('Basic '):
             return None
 
-        return authorization_header[len("Basic "):]
+        token = authorization_header.split(' ')[-1]
+        return token
 
-    def decode_base64_authorization_header(self, base64_authorization_header:
-                                           str) -> str:
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """_summary_
 
+        Args:
+                 base64_authorization_header (str): _description_
+
+        Returns:
+                  str: _description_
+        """
         if base64_authorization_header is None:
             return None
         if not isinstance(base64_authorization_header, str):
             return None
+
         try:
-            decoded_bytes = base64.b64decode(base64_authorization_header)
-            return decoded_bytes.decode('utf-8')
+            item_to_decode = base64_authorization_header.encode('utf-8')
+            decoded = base64.b64decode(item_to_decode)
+            return decoded.decode('utf-8')
         except Exception:
             return None
