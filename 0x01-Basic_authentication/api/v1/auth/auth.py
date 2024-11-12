8ad8ca7b1a6b13c0e52507ba89413210ecc87cba
beloveyebaoh <beloveyeboah@gmail.com>
2024-11-12 22:26:48 +0000

    auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Template class for all authentication systems"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if authentication is required
+        :param path: string path to be checked
+        :param excluded_paths: list of paths that do not require authentication
+        :return: False for now, implementation will be updated later
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Retrieves the authorization header from the request
+        :param request: Flask request object
+        :return: None for now, implementation will be updated later
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the current user from the request
+        :param request: Flask request object
+        :return: None for now, implementation will be updated later
+        """
+        return None
