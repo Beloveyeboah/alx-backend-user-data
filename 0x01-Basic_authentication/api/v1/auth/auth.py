a593440d55b8134c3850f8c385dcb3bc002ee604
beloveyebaoh <beloveyeboah@gmail.com>
2024-11-12 22:44:40 +0000

    routes don't

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,15 +9,26 @@ from typing import List, TypeVar
 
 class Auth:
     """Template class for all authentication systems"""
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-        Determines if authentication is required
+        """ Determines if authentication is required
         :param path: string path to be checked
-        :param excluded_paths: list of paths that do not require authentication
-        :return: False for now, implementation will be updated later
-        """
-        return False
+        :param excluded_paths: list of paths that do
+        not require authentication
+        return: True if authentication is required,
+        False otherwise """
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+        # Normalize the path to ensure it has a trailing slash
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/') and path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
