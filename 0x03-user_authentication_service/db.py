7600ce3d6f52213f7048fe6adb690269cbef4cb7
beloveyebaoh <beloveyeboah@gmail.com>
2024-11-21 04:16:52 +0000

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
 
 from user import Base
 from user import User
@@ -37,3 +40,17 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ search for a user in the database
+        """
+
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if not user:
+                raise NoResultFound
+            return user
+        except NoResultFound:
+            raise NoResultFound("No user found with the given criteria")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid request parameters")
